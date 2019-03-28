5c5c3d532fdcf5eebbe3396ad39b4367941c050a
Ashton Becher <ash.becher@gmail.com>
2019-03-28 08:43:38 -0500

    Add project files.

---
+++ C01/simpleCalcFrm.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace C01
+{
+    partial class simpleCalcFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.op1Lbl = new System.Windows.Forms.Label();
+            this.op2Lbl = new System.Windows.Forms.Label();
+            this.op1TxtBox = new System.Windows.Forms.TextBox();
+            this.op2TxtBox = new System.Windows.Forms.TextBox();
+            this.divideLine = new System.Windows.Forms.Label();
+            this.resultLbl = new System.Windows.Forms.Label();
+            this.resultTxtBox = new System.Windows.Forms.TextBox();
+            this.myNameLbl = new System.Windows.Forms.Label();
+            this.keysLbl = new System.Windows.Forms.Label();
+            this.groupBox = new System.Windows.Forms.Label();
+            this.operationsLbl = new System.Windows.Forms.Label();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.subtractBtn = new System.Windows.Forms.Button();
+            this.multBtn = new System.Windows.Forms.Button();
+            this.divideBtn = new System.Windows.Forms.Button();
+            this.functionsLbl = new System.Windows.Forms.Label();
+            this.minBtn = new System.Windows.Forms.Button();
+            this.maxBtn = new System.Windows.Forms.Button();
+            this.Sqrt = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLbl
+            // 
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Font = new System.Drawing.Font("Papyrus", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLbl.ForeColor = System.Drawing.Color.Purple;
+            this.titleLbl.Location = new System.Drawing.Point(12, 9);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(222, 27);
+            this.titleLbl.TabIndex = 0;
+            this.titleLbl.Text = "Ashton Becher\'s Calculator";
+            // 
+            // op1Lbl
+            // 
+            this.op1Lbl.AutoSize = true;
+            this.op1Lbl.Location = new System.Drawing.Point(13, 77);
+            this.op1Lbl.Name = "op1Lbl";
+            this.op1Lbl.Size = new System.Drawing.Size(122, 17);
+            this.op1Lbl.TabIndex = 1;
+            this.op1Lbl.Text = "Enter Operand 1: ";
+            // 
+            // op2Lbl
+            // 
+            this.op2Lbl.AutoSize = true;
+            this.op2Lbl.Location = new System.Drawing.Point(13, 119);
+            this.op2Lbl.Name = "op2Lbl";
+            this.op2Lbl.Size = new System.Drawing.Size(122, 17);
+            this.op2Lbl.TabIndex = 2;
+            this.op2Lbl.Text = "Enter Operand 2: ";
+            // 
+            // op1TxtBox
+            // 
+            this.op1TxtBox.Location = new System.Drawing.Point(141, 74);
+            this.op1TxtBox.MaxLength = 5;
+            this.op1TxtBox.Name = "op1TxtBox";
+            this.op1TxtBox.Size = new System.Drawing.Size(100, 22);
+            this.op1TxtBox.TabIndex = 1;
+            // 
+            // op2TxtBox
+            // 
+            this.op2TxtBox.Location = new System.Drawing.Point(141, 116);
+            this.op2TxtBox.MaxLength = 5;
+            this.op2TxtBox.Name = "op2TxtBox";
+            this.op2TxtBox.Size = new System.Drawing.Size(100, 22);
+            this.op2TxtBox.TabIndex = 2;
+            // 
+            // divideLine
+            // 
+            this.divideLine.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.divideLine.Location = new System.Drawing.Point(74, 160);
+            this.divideLine.Name = "divideLine";
+            this.divideLine.Size = new System.Drawing.Size(167, 3);
+            this.divideLine.TabIndex = 3;
+            // 
+            // resultLbl
+            // 
+            this.resultLbl.AutoSize = true;
+            this.resultLbl.Location = new System.Drawing.Point(56, 195);
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Size = new System.Drawing.Size(48, 17);
+            this.resultLbl.TabIndex = 4;
+            this.resultLbl.Text = "Result";
+            // 
+            // resultTxtBox
+            // 
+            this.resultTxtBox.Location = new System.Drawing.Point(141, 192);
+            this.resultTxtBox.MaxLength = 5;
+            this.resultTxtBox.Name = "resultTxtBox";
+            this.resultTxtBox.ReadOnly = true;
+            this.resultTxtBox.Size = new System.Drawing.Size(100, 22);
+            this.resultTxtBox.TabIndex = 12;
+            // 
+            // myNameLbl
+            // 
+            this.myNameLbl.AutoSize = true;
+            this.myNameLbl.Location = new System.Drawing.Point(13, 286);
+            this.myNameLbl.Name = "myNameLbl";
+            this.myNameLbl.Size = new System.Drawing.Size(101, 17);
+            this.myNameLbl.TabIndex = 6;
+            this.myNameLbl.Text = "Ashton Becher";
+            // 
+            // keysLbl
+            // 
+            this.keysLbl.AutoSize = true;
+            this.keysLbl.Location = new System.Drawing.Point(297, 31);
+            this.keysLbl.Name = "keysLbl";
+            this.keysLbl.Size = new System.Drawing.Size(39, 17);
+            this.keysLbl.TabIndex = 7;
+            this.keysLbl.Text = "Keys";
+            // 
+            // groupBox
+            // 
+            this.groupBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.groupBox.Location = new System.Drawing.Point(289, 48);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(189, 214);
+            this.groupBox.TabIndex = 8;
+            // 
+            // operationsLbl
+            // 
+            this.operationsLbl.AutoSize = true;
+            this.operationsLbl.Location = new System.Drawing.Point(297, 49);
+            this.operationsLbl.Name = "operationsLbl";
+            this.operationsLbl.Size = new System.Drawing.Size(78, 17);
+            this.operationsLbl.TabIndex = 9;
+            this.operationsLbl.Text = "Operations";
+            // 
+            // addBtn
+            // 
+            this.addBtn.AutoSize = true;
+            this.addBtn.Location = new System.Drawing.Point(300, 72);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(53, 27);
+            this.addBtn.TabIndex = 3;
+            this.addBtn.Text = "+";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // subtractBtn
+            // 
+            this.subtractBtn.AutoSize = true;
+            this.subtractBtn.Location = new System.Drawing.Point(390, 74);
+            this.subtractBtn.Name = "subtractBtn";
+            this.subtractBtn.Size = new System.Drawing.Size(55, 27);
+            this.subtractBtn.TabIndex = 4;
+            this.subtractBtn.Text = "-";
+            this.subtractBtn.UseVisualStyleBackColor = true;
+            this.subtractBtn.Click += new System.EventHandler(this.subtractBtn_Click);
+            // 
+            // multBtn
+            // 
+            this.multBtn.AutoSize = true;
+            this.multBtn.Location = new System.Drawing.Point(300, 114);
+            this.multBtn.Name = "multBtn";
+            this.multBtn.Size = new System.Drawing.Size(53, 27);
+            this.multBtn.TabIndex = 5;
+            this.multBtn.Text = "*";
+            this.multBtn.UseVisualStyleBackColor = true;
+            this.multBtn.Click += new System.EventHandler(this.multBtn_Click);
+            // 
+            // divideBtn
+            // 
+            this.divideBtn.AutoSize = true;
+            this.divideBtn.Location = new System.Drawing.Point(390, 114);
+            this.divideBtn.Name = "divideBtn";
+            this.divideBtn.Size = new System.Drawing.Size(55, 27);
+            this.divideBtn.TabIndex = 6;
+            this.divideBtn.Text = "/";
+            this.divideBtn.UseVisualStyleBackColor = true;
+            this.divideBtn.Click += new System.EventHandler(this.divideBtn_Click);
+            // 
+            // functionsLbl
+            // 
+            this.functionsLbl.AutoSize = true;
+            this.functionsLbl.Location = new System.Drawing.Point(297, 160);
+            this.functionsLbl.Name = "functionsLbl";
+            this.functionsLbl.Size = new System.Drawing.Size(69, 17);
+            this.functionsLbl.TabIndex = 14;
+            this.functionsLbl.Text = "Functions";
+            // 
+            // minBtn
+            // 
+            this.minBtn.AutoSize = true;
+            this.minBtn.Location = new System.Drawing.Point(300, 189);
+            this.minBtn.Name = "minBtn";
+            this.minBtn.Size = new System.Drawing.Size(53, 27);
+            this.minBtn.TabIndex = 7;
+            this.minBtn.Text = "Min";
+            this.minBtn.UseVisualStyleBackColor = true;
+            this.minBtn.Click += new System.EventHandler(this.minBtn_Click);
+            // 
+            // maxBtn
+            // 
+            this.maxBtn.AutoSize = true;
+            this.maxBtn.Location = new System.Drawing.Point(390, 189);
+            this.maxBtn.Name = "maxBtn";
+            this.maxBtn.Size = new System.Drawing.Size(55, 27);
+            this.maxBtn.TabIndex = 8;
+            this.maxBtn.Text = "Max";
+            this.maxBtn.UseVisualStyleBackColor = true;
+            this.maxBtn.Click += new System.EventHandler(this.maxBtn_Click);
+            // 
+            // Sqrt
+            // 
+            this.Sqrt.AutoSize = true;
+            this.Sqrt.Location = new System.Drawing.Point(300, 218);
+            this.Sqrt.Name = "Sqrt";
+            this.Sqrt.Size = new System.Drawing.Size(53, 27);
+            this.Sqrt.TabIndex = 9;
+            this.Sqrt.Text = "Sqrt";
+            this.Sqrt.UseVisualStyleBackColor = true;
+            this.Sqrt.Click += new System.EventHandler(this.Sqrt_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.AutoSize = true;
+            this.clearBtn.Location = new System.Drawing.Point(300, 280);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 27);
+            this.clearBtn.TabIndex = 10;
+            this.clearBtn.Text = "&Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.AutoSize = true;
+            this.exitBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitBtn.Location = new System.Drawing.Point(390, 280);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 27);
+            this.exitBtn.TabIndex = 11;
+            this.exitBtn.Text = "E&xit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // simpleCalcFrm
+            // 
+            this.AcceptButton = this.clearBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitBtn;
+            this.ClientSize = new System.Drawing.Size(513, 334);
+            this.ControlBox = false;
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.Sqrt);
+            this.Controls.Add(this.maxBtn);
+            this.Controls.Add(this.minBtn);
+            this.Controls.Add(this.functionsLbl);
+            this.Controls.Add(this.divideBtn);
+            this.Controls.Add(this.multBtn);
+            this.Controls.Add(this.subtractBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.operationsLbl);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.keysLbl);
+            this.Controls.Add(this.myNameLbl);
+            this.Controls.Add(this.resultTxtBox);
+            this.Controls.Add(this.resultLbl);
+            this.Controls.Add(this.divideLine);
+            this.Controls.Add(this.op2TxtBox);
+            this.Controls.Add(this.op1TxtBox);
+            this.Controls.Add(this.op2Lbl);
+            this.Controls.Add(this.op1Lbl);
+            this.Controls.Add(this.titleLbl);
+            this.Name = "simpleCalcFrm";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.Label op1Lbl;
+        private System.Windows.Forms.Label op2Lbl;
+        private System.Windows.Forms.TextBox op1TxtBox;
+        private System.Windows.Forms.TextBox op2TxtBox;
+        private System.Windows.Forms.Label divideLine;
+        private System.Windows.Forms.Label resultLbl;
+        private System.Windows.Forms.TextBox resultTxtBox;
+        private System.Windows.Forms.Label myNameLbl;
+        private System.Windows.Forms.Label keysLbl;
+        private System.Windows.Forms.Label groupBox;
+        private System.Windows.Forms.Label operationsLbl;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button subtractBtn;
+        private System.Windows.Forms.Button multBtn;
+        private System.Windows.Forms.Button divideBtn;
+        private System.Windows.Forms.Label functionsLbl;
+        private System.Windows.Forms.Button minBtn;
+        private System.Windows.Forms.Button maxBtn;
+        private System.Windows.Forms.Button Sqrt;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ C01/simpleCalcFrm.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C01
+{
+    // Name: Ashton Becher
+    // Date: January 27, 2019
+    // Project: C01 - Simple Calculator
+    /* Decsription: Windows form that is a basic calculator. Functions such as basic math as well
+     as minimum, maximum, and square root also provided */
+
+    public partial class simpleCalcFrm : Form
+    {
+        // Global variable declaration to reduce repetition in code
+        int counterFunc = 0;
+        int counterOp = 0;
+        decimal op1 = 0;
+        decimal op2 = 0;
+        decimal result = 0;
+        decimal roundedResult = 0;
+
+        public simpleCalcFrm()
+        {
+            InitializeComponent();
+        }     
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            String text = "Total number of Operations:\t" + counterOp.ToString() + "\n" + 
+                "Total number of Functions:\t\t" + counterFunc.ToString() + "\n" +
+                "Total Number of Calculations: \t" + (counterFunc + counterOp).ToString();
+            String title = "Summary for Ashton Becher's Calculator";
+            MessageBox.Show(text, title);
+            this.Close();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            op1 = Convert.ToDecimal(op1TxtBox.Text);
+            op2 = Convert.ToDecimal(op2TxtBox.Text);
+            result = op1 + op2;
+            roundedResult = Math.Round(result, 3);
+            resultTxtBox.Text = roundedResult.ToString();
+
+            counterOp += 1;
+            op1TxtBox.Focus();
+            op1TxtBox.SelectAll();
+            op2TxtBox.SelectAll();
+        }
+        
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            op1TxtBox.Clear();
+            op2TxtBox.Clear();
+            resultTxtBox.Clear();
+            op1TxtBox.Focus();
+            counterFunc = 0;
+            counterOp = 0;
+        }
+
+        private void subtractBtn_Click(object sender, EventArgs e)
+        {
+            op1 = Convert.ToDecimal(op1TxtBox.Text);
+            op2 = Convert.ToDecimal(op2TxtBox.Text);
+            result = op1 - op2;
+            roundedResult = Math.Round(result, 3);
+            resultTxtBox.Text = roundedResult.ToString();
+
+            counterOp += 1;
+            op1TxtBox.Focus();
+            op1TxtBox.SelectAll();
+            op2TxtBox.SelectAll();
+        }
+
+        private void multBtn_Click(object sender, EventArgs e)
+        {
+            op1 = Convert.ToDecimal(op1TxtBox.Text);
+            op2 = Convert.ToDecimal(op2TxtBox.Text);
+            result = op1 * op2;
+            roundedResult = Math.Round(result, 3);
+            resultTxtBox.Text = roundedResult.ToString();
+
+            counterOp += 1;
+            op1TxtBox.Focus();
+            op1TxtBox.SelectAll();
+            op2TxtBox.SelectAll();
+        }
+
+        private void divideBtn_Click(object sender, EventArgs e)
+        {
+            op1 = Convert.ToDecimal(op1TxtBox.Text);
+            op2 = Convert.ToDecimal(op2TxtBox.Text);
+            result = op1 / op2;
+            roundedResult = Math.Round(result, 3);
+            resultTxtBox.Text = roundedResult.ToString();
+
+            counterOp += 1;
+            op1TxtBox.Focus();
+            op1TxtBox.SelectAll();
+            op2TxtBox.SelectAll();
+        }
+
+        private void minBtn_Click(object sender, EventArgs e)
+        {
+            op1 = Convert.ToDecimal(op1TxtBox.Text);
+            op2 = Convert.ToDecimal(op2TxtBox.Text);
+            result = Math.Min(op1, op2);
+            roundedResult = Math.Round(result, 3);
+            resultTxtBox.Text = roundedResult.ToString();
+
+            counterFunc += 1;
+            op1TxtBox.Focus();
+            op1TxtBox.SelectAll();
+            op2TxtBox.SelectAll();
+        }
+
+        private void maxBtn_Click(object sender, EventArgs e)
+        {
+            op1 = Convert.ToDecimal(op1TxtBox.Text);
+            op2 = Convert.ToDecimal(op2TxtBox.Text);
+            result = Math.Max(op1, op2);
+            roundedResult = Math.Round(result, 3);
+            resultTxtBox.Text = roundedResult.ToString();
+
+            counterFunc += 1;
+            op1TxtBox.Focus();
+            op1TxtBox.SelectAll();
+            op2TxtBox.SelectAll();
+        }
+
+        private void Sqrt_Click(object sender, EventArgs e)
+        {
+            double doubleOp1 = Convert.ToDouble(op1TxtBox.Text);
+            double tempResult = Math.Sqrt(doubleOp1);
+            result = Convert.ToDecimal(tempResult);
+            roundedResult = Math.Round(result, 3);
+            resultTxtBox.Text = roundedResult.ToString();
+
+            counterFunc += 1;
+            op1TxtBox.Focus();
+            op1TxtBox.SelectAll();
+            op2TxtBox.SelectAll();
+        }
+    }
+}
